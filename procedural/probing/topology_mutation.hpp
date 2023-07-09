e67fafe66b7e1cffb983e759f92a57e43a14d4c4
Chifeng Wen <daviesx66@gmail.com>
2023-07-09 21:20:47 +0800

    Saving current work.

---
+++ procedural/probing/topology_mutation.hpp
@@ -0,0 +1,65 @@
+// e8City
+// Copyright (C) 2023 e8yes
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "procedural/probing/topology_definition.hpp"
+#include <tuple>
+
+namespace e8 {
+namespace procedural {
+
+//
+using Edge = std::tuple<unsigned, unsigned>;
+
+//
+struct Mutation {
+  //
+  std::vector<Edge> additions;
+
+  //
+  std::vector<Edge> deletions;
+};
+
+//
+class EdgeSetState {
+public:
+  EdgeSetState();
+  ~EdgeSetState() = default;
+
+  //
+  Mutation const &Mutate(unsigned mutation_count);
+
+  //
+  void Revert();
+
+  //
+  std::vector<Edge> ActiveEdges() const;
+  std::vector<Edge> DeletedEdges() const;
+
+private:
+  std::vector<Edge> edges_;
+  unsigned separator_;
+  Mutation current_mutation_;
+};
+
+//
+class MutationRecovery {
+public:
+};
+
+} // namespace procedural
+} // namespace e8
