d3592219fe375d07426fd1d797e0a8ba6bdd3fcc
Chifeng Wen <daviesx66@gmail.com>
2023-07-09 12:24:07 +0800

    Runs python tests int the build script. (#6)

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Builds and tests C++ binaries.
 mkdir -p bin
 pushd bin
 cmake ..
 make -j `nproc`
 find . -type f -name '*_test' -exec {} \;
 popd
+
+# Tests Python modules.
+python3 -m procedural.probing.population_test
