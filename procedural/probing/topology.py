194b67c4b71a58975643c7d55968ae9b42975dfe
Davis Wen <daviesx66@gmail.com>
2023-08-01 19:00:58 +0800

    formating.

--- procedural/probing/topology.py
+++ procedural/probing/topology.py
@@ -1,21 +1,21 @@
-""" 
- e8City 
- Copyright (C) 2023 e8yes 
-  
- This program is free software: you can redistribute it and/or modify 
- it under the terms of the GNU General Public License as published by 
- the Free Software Foundation, either version 3 of the License, or 
- (at your option) any later version. 
-  
- This program is distributed in the hope that it will be useful, 
- but WITHOUT ANY WARRANTY; without even the implied warranty of 
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- GNU General Public License for more details. 
-  
- You should have received a copy of the GNU General Public License 
- along with this program.  If not, see <http://www.gnu.org/licenses/>. 
- """ 
- 
+"""
+ e8City
+ Copyright (C) 2023 e8yes
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
 import e8citydll
 from procedural.probing.population import PopulationProbe
 from dataclasses import dataclass
@@ -24,7 +24,8 @@ from typing import List
 
 @dataclass
 class ProbeConnection:
-    """In the order of the specified probe array, this indicate two probes at the following indices should be connected bidirectionally.
+    """In the order of the specified probe array, this indicate two probes at
+    the following indices should be connected bidirectionally.
     """
     src_probe_index: int
     dst_probe_index: int
@@ -38,7 +39,8 @@ class ProbeTopology:
     # The unique connections between the population probes.
     connections: List[ProbeConnection]
 
-    # The objective score of the connection. It estimates the average number of residents transported at the probes.
+    # The objective score of the connection. It estimates the average number
+    # of residents transported at the probes.
     score: float
 
 
@@ -65,11 +67,14 @@ def _ToProbeTopology(
 def ComputePopulationProbeTopology(
         probes: List[PopulationProbe],
         optimization_step_count: int) -> ProbeTopology:
-    """It computes the connections amongst the specified population probes such that the transportation between any two probes is reasonably efficient.
+    """It computes the connections amongst the specified population probes
+        such that the transportation between any two probes is reasonably
+        efficient.
 
     Args:
         probes (List[PopulationProbe]): The population probes to be connected.
-        optimization_step_count (int): The number of optimization steps to take to produce the topology.
+        optimization_step_count (int): The number of optimization steps to
+            take to produce the topology.
 
     Returns:
         ProbeTopology: See the above data class.
