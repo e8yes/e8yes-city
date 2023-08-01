e61f3b21bfbf23b0900db9b403b67d55ecb15b82
Chifeng Wen <daviesx66@gmail.com>
2023-08-01 18:41:43 +0800

    Update topology.py

--- procedural/probing/topology.py
+++ procedural/probing/topology.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
 import e8citydll
-import pickle
 from procedural.probing.population import PopulationProbe
-from procedural.probing.population import GeneratePopulationProbes
 from dataclasses import dataclass
 from typing import List
 
@@ -67,24 +64,3 @@ def ComputePopulationProbeTopology(
     internal_result = e8citydll.ComputeProbeTopology(
         internal_probes, optimization_step_count)
     return _ToProbeTopology(internal_result)
-
-
-probes = GeneratePopulationProbes(city_size=5000)
-print(len(probes))
-
-result = ComputePopulationProbeTopology(probes, 100000)
-print(result.score)
-print(len(result.connections))
-
-# f = open("optimized_topology", mode="wb")
-# pickle.dump(result, f)
-# f = open("optimized_topology.bak3", mode="rb")
-# result:  ProbeTopology = pickle.load(f)
-# print(result)
-
-
-for connection in result.connections:
-    plt.plot([probes[connection.src_probe_index].location[0], probes[connection.dst_probe_index].location[0]],
-             [probes[connection.src_probe_index].location[1], probes[connection.dst_probe_index].location[1]], color="grey", linewidth=0.3)
-
-plt.show()
